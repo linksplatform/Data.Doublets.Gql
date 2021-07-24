322bd5a8accbaedca2db5284072b48cac56b0f2f
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-07-24 15:35:51 +0300

    Add files via upload

--- csharp/Platform.Data.Doublets.GraphQL.Schema/LinkType.cs
+++ csharp/Platform.Data.Doublets.GraphQL.Schema/LinkType.cs
@@ -1,5 +1,7 @@
+using System.Net.Http.Headers;
 using LinksStorage;
 using GraphQL.Types;
+using Platform.Data.Doublets;
 
 namespace GraphQL.Samples.Schemas.Chat
 {
@@ -9,17 +11,28 @@ namespace GraphQL.Samples.Schemas.Chat
         {
             Field(o => o.Id);
             Field(o => o.from_id);
-            Field(o => o.from, type: typeof(LinkType));
-            Field(o => o.to, type: typeof(LinkType));
+            Field(o => o.from, false, type: typeof(LinkType)).Resolve(ResolveFrom);
+            Field(o => o.to, type: typeof(LinkType)).Resolve(ResolveTo);
             Field(o => o.to_id);
-            Field(o => o.type, type: typeof(LinkType));
+            Field(o => o.type, type: typeof(LinkType)).Resolve(ResolveType);
             Field(o => o.type_id);
         }
 
-        //private MessageFrom ResolveFrom(IResolveFieldContext<Message> context)
-        //{
-        //    var message = context.Source;
-        //    return message.From;
-        //}
+        private Link ResolveFrom(IResolveFieldContext<Link> context)
+        {
+            var link = context.Source;
+            return link.from ?? new Link();
+        }
+
+        private Link ResolveTo(IResolveFieldContext<Link> context)
+        {
+            var link = context.Source;
+            return link.to ?? new Link();
+        }
+        private Link ResolveType(IResolveFieldContext<Link> context)
+        {
+            var link = context.Source;
+            return link.type ?? new Link();
+        }
     }
 }
