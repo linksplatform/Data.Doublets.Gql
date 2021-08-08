fa944301573814623e79cc94e967920e663d41b2
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-08-08 18:29:57 +0300

    Add files via upload

--- csharp/Platform.Data.Doublets.GraphQL.Schema/Input/OrderByInputType.cs
+++ csharp/Platform.Data.Doublets.GraphQL.Schema/Input/OrderByInputType.cs
@@ -1,28 +1,20 @@
-﻿using GraphQL.Types;
+﻿using GraphQL.Samples.Schemas.Link;
+using GraphQL.Types;
 
 namespace Input
 {
     class OrderByInputType : InputObjectGraphType
     {
-        public enum order_by
-        {
-            asc,
-            asc_nulls_first,
-            asc_nulls_last,
-            desc,
-            desc_nulls_first,
-            desc_nulls_lasr
-        }
         public OrderByInputType()
         {
-            //Field<EnumerationGraphType<order_by>>("order_by");
+            //Field<OrderByEnum>("order_by");
             Field<OrderByInputType>("from");
-            Field<EnumerationGraphType<order_by>>("from_id");
-            Field<EnumerationGraphType<order_by>>("id");
+            Field<OrderByEnum>("from_id");
+            Field<OrderByEnum>("id");
             Field<OrderByInputType>("to");
-            Field<EnumerationGraphType<order_by>>("to_id");
+            Field<OrderByEnum>("to_id");
             Field<OrderByInputType>("type");
-            Field<EnumerationGraphType<order_by>>("type_id");
+            Field<OrderByEnum>("type_id");
         }
     }
 }
---
+++ csharp/Platform.Data.Doublets.GraphQL.Schema/OrderByEnum.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQL.Samples.Schemas.Link
+{
+    class OrderByEnum : EnumerationGraphType<order_by>
+    {
+        public OrderByEnum()
+        {
+            Name = "OrderByEnum";
+        }
+    }
+}
