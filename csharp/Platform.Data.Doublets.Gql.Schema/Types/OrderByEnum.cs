4b102dbd747240aedb4002a513ac861507fc23d6
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-08-22 15:56:20 +0300

    Delete csharp/Platform.Data.Doublets.Gql.Schema/Types directory

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/OrderByEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿using GraphQL.Types;
-
-namespace Platform.Data.Doublets.Gql.Schema
-{
-    internal class OrderByEnum : EnumerationGraphType<order_by>
-    {
-        public OrderByEnum() => Name = "OrderByEnum";
-    }
-}
