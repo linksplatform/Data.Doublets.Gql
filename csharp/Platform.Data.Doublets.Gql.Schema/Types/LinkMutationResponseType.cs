1b995233ea87bd02da51265d2dc350b563eb8e74
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-08-22 15:56:54 +0300

    Add files via upload

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/LinkMutationResponseType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    internal class LinksMutationResponseType : ObjectGraphType
+    {
+        public LinksMutationResponseType()
+        {
+            Field<IntGraphType>("affected_rows");
+            Field<ListGraphType<LinkType>>("returning");
+        }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/SumFieldsType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types
+{
+    class SumFieldsType
+    {
+        public long from_id { get; set; }
+
+        public long id { get; set; }
+
+        public long to_id { get; set; }
+
+        public long type_id { get; set; }
+    }
+}
