04918b815314af61b6fa0df8260432926df323cd
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-08-23 14:34:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Platform.IO;
+using Serilog;
+using Serilog.Events;
+using System;
+
+namespace Platform.Data.Doublets.Gql.Server
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                var dbFileName = ConsoleHelpers.GetOrReadArgument(0, $"Document name (default: {Startup.DbFileName})", args);
+                if (!string.IsNullOrWhiteSpace(dbFileName))
+                {
+                    Startup.DbFileName = dbFileName;
+                }
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseSerilog()
+                        .UseStartup<Startup>();
+                });
+    }
+}
