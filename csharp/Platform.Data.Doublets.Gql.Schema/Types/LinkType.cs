d68f2c5e77fcbec737537c9d9917dedb0f257af6
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2021-08-27 15:18:55 +0300

    Update LinkType.cs

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/LinkType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/LinkType.cs
@@ -10,8 +10,8 @@ namespace Platform.Data.Doublets.Gql.Schema.Types
     {
         public LinkType()
         {
-            Field(o => o.id);
-            Field(o => o.from_id, nullable: true);
+            Field(o => o.id, nullable: true);
+            Field(o => o.from_id);
             Field(o => o.from, type: typeof(LinkType)).Resolve(ResolveFrom);
             Field(o => o.to, type: typeof(LinkType)).Resolve(ResolveTo);
             Field(o => o.to_id, nullable: true);
