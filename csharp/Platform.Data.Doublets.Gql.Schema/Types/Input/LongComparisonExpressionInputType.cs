f80a82805fd924ffdd87332502357de0ed8644b7
konard <drakonard@gmail.com>
2021-09-15 17:28:27 +0300

    An example of source schema embedding is added.

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LongComparisonExpressionInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LongComparisonExpressionInputType.cs
@@ -2,6 +2,22 @@
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Input
 {
+    /// <remarks>
+    /// """
+    /// expression to compare columns of type bigint. All fields are combined with logical 'AND'.
+    /// """
+    /// input bigint_comparison_exp {
+    ///   _eq: bigint
+    ///   _gt: bigint
+    ///   _gte: bigint
+    ///   _in: [bigint!]
+    ///   _is_null: Boolean
+    ///   _lt: bigint
+    ///   _lte: bigint
+    ///   _neq: bigint
+    ///   _nin: [bigint!]
+    /// }
+    /// </remarks>
     internal class LongComparisonExpressionInputType : InputObjectGraphType<LongComparisonExpression>
     {
         public LongComparisonExpressionInputType()
