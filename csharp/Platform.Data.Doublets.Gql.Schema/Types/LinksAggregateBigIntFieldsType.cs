01305591ebe85612c2593709863b4289067f218c
FreePhoenix <freephoenix888@gmail.com>
2021-09-17 19:20:40 +0600

    Update remarks

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksAggregateBigIntFieldsType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksAggregateBigIntFieldsType.cs
@@ -7,6 +7,41 @@ using System.Threading.Tasks;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Input
 {
+    /// <remarks>
+    /// """aggregate max on columns"""
+    /// type links_max_fields {
+    ///   from_id: bigint
+    ///   id: bigint
+    ///   to_id: bigint
+    ///   type_id: bigint
+    /// }
+    ///
+    /// """aggregate min on columns"""
+    /// type links_min_fields {
+    ///   from_id: bigint
+    ///   id: bigint
+    ///   to_id: bigint
+    ///   type_id: bigint
+    /// }
+    ///
+    /// """
+    /// input type for updating data in table "links"
+    /// """
+    /// input links_set_input {
+    ///   from_id: bigint
+    ///   id: bigint
+    ///   to_id: bigint
+    ///   type_id: bigint
+    /// }
+    ///
+    /// """aggregate sum on columns"""
+    /// type links_sum_fields {
+    ///   from_id: bigint
+    ///   id: bigint
+    ///   to_id: bigint
+    ///   type_id: bigint
+    /// }
+    /// </remarks>
     class LinksAggregateBigIntFieldsType : ObjectGraphType
     {
         public LinksAggregateBigIntFieldsType()
