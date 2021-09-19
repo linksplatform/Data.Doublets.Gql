613d85eb3db0813aaca652e9d8b3959612b65ada
FreePhoenix <freephoenix888@gmail.com>
2021-09-19 16:31:12 +0600

    Update LinksFieldsOrderByInputType constructors

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/LinksUpdateColumnEnumType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/LinksUpdateColumnEnumType.cs
@@ -1,10 +1,3 @@
-using GraphQL.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Platform.Data.Doublets.Gql.Schema.Types.Enum
 {
     /// <remarks>
--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
@@ -4,12 +4,7 @@ namespace Platform.Data.Doublets.Gql.Schema.Types.Input
 {
     public class LinksFieldsOrderByInputType : InputObjectGraphType<LinksFieldsOrderBy>
     {
-        public LinksFieldsOrderByInputType()
-        {
-            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
-            Field(x => x.from_id, nullable: true, type: typeof(OrderByEnumType));
-            Field(x => x.to_id, nullable: true, type: typeof(OrderByEnumType));
-            Field(x => x.type_id, nullable: true, type: typeof(OrderByEnumType));
-        }
+        public LinksFieldsOrderByInputType(){}
+        public LinksFieldsOrderByInputType(string name) => Name = name;
     }
 }
