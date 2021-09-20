2b34be749be78a441e3221f6ff2b14455014f1a6
FreePhoenix <freephoenix888@gmail.com>
2021-09-20 22:38:03 +0600

    Remove redundant code

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksOnConflictInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksOnConflictInputType.cs
@@ -1,9 +1,4 @@
 ﻿using GraphQL.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Platform.Data.Doublets.Gql.Schema.Types.Enum;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Input
