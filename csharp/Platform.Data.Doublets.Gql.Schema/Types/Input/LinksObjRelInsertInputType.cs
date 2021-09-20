9d5a5e73dade336aa669fd9c6f22d6997b349e05
FreePhoenix <freephoenix888@gmail.com>
2021-09-20 22:38:11 +0600

    Remove redundant code

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksObjRelInsertInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksObjRelInsertInputType.cs
@@ -1,10 +1,4 @@
 ﻿using GraphQL.Types;
-using Platform.Data.Doublets.Gql.Schema.Types.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Input
 {
