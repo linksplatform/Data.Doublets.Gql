e0f6cb355a42489d86aadaee666676ab935fbbf3
FreePhoenix <freephoenix888@gmail.com>
2021-09-22 10:52:32 +0600

    Update LinksAggregateBigIntFieldsType

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksAggregateBigIntFieldsType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksAggregateBigIntFieldsType.cs
@@ -2,7 +2,7 @@
 
 namespace Platform.Data.Doublets.Gql.Schema.Types
 {
-    public class LinksAggregateBigIntFieldsType : ObjectGraphType
+    public class LinksAggregateBigIntFieldsType : ObjectGraphType<LinksAggregateBigIntFields>
     {
         public LinksAggregateBigIntFieldsType()
         {
@@ -11,6 +11,10 @@ namespace Platform.Data.Doublets.Gql.Schema.Types
         public LinksAggregateBigIntFieldsType(string name)
         {
             Name = name;
+            Field(x => x.id, nullable: true, type: typeof(LongGraphType));
+            Field(x => x.from_id, nullable: true, type: typeof(LongGraphType));
+            Field(x => x.to_id, nullable: true, type: typeof(LongGraphType));
+            Field(x => x.type_id, nullable: true, type: typeof(LongGraphType));
         }
     }
 }
