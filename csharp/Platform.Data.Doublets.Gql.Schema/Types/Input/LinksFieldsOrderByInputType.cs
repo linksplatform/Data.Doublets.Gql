f3630784f110b223230271c8ecb9e44850b3c274
FreePhoenix <freephoenix888@gmail.com>
2021-09-24 19:15:49 +0600

    Add fields to LinksFieldsOrderByInputType

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.Types;
+using Platform.Data.Doublets.Gql.Schema.Types.Enum;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Input
 {
@@ -11,6 +12,10 @@ namespace Platform.Data.Doublets.Gql.Schema.Types.Input
         public LinksFieldsOrderByInputType(string name)
         {
             Name = name;
+            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.from_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.to_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.type_id, nullable: true, type: typeof(OrderByEnumType));
         }
     }
 }
