93f41465e8c3a03a70516d79fa11c51608ed3672
FreePhoenix <freephoenix888@gmail.com>
2021-09-24 19:16:20 +0600

    Update fields positions

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/LinksFieldsOrderByInputType.cs
@@ -12,8 +12,8 @@ namespace Platform.Data.Doublets.Gql.Schema.Types.Input
         public LinksFieldsOrderByInputType(string name)
         {
             Name = name;
-            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
             Field(x => x.from_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
             Field(x => x.to_id, nullable: true, type: typeof(OrderByEnumType));
             Field(x => x.type_id, nullable: true, type: typeof(OrderByEnumType));
         }
