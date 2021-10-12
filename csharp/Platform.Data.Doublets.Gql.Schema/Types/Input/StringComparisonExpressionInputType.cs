18f9d6cd049fddf53d8c9e80f47e766ee270acf1
FreePhoenix <freephoenix888@gmail.com>
2021-10-12 19:46:09 +0600

    Update String_comparison_exp

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/StringComparisonExpressionInputType.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using GraphQL.Types;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    public class StringComparisonExpressionInputType : InputObjectGraphType<StringComparisonExpression>
+    {
+        public StringComparisonExpressionInputType()
+        {
+            Name = "String_comparison_exp";
+            Field(x => x._eq, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._gt, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._gte, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._ilike, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._in, nullable: true, type: typeof(List<NonNullGraphType<StringGraphType>>));
+            Field(x => x._is_null, nullable: true, type: typeof(bool));
+            Field(x => x._like, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._lt, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._lte, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._neq, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._nilike, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._nin, nullable: true, type: typeof(List<NonNullGraphType<StringGraphType>>));
+            Field(x => x._nlike, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._nsimilar, nullable: true, type: typeof(StringGraphType));
+            Field(x => x._similar, nullable: true, type: typeof(StringGraphType));
+        }
+    }
+}
