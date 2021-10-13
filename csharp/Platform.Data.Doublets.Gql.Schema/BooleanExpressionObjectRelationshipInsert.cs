d8c2396a6fc6c11f5cd6fe1b3d72cb8ee1f3bfd8
FreePhoenix <freephoenix888@gmail.com>
2021-10-13 23:17:53 +0600

    Update bool_exp_obj_rel_insert_input

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/BooleanExpressionObjectRelationshipInsert.cs
@@ -0,0 +1,8 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    public class BooleanExpressionObjectRelationshipInsert
+    {
+        public BooleanExpressionInsert data { get; set; }
+        public BooleanExpressionOnConflict? on_conflict { get; set; }
+    }
+}
