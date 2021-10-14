40948be40b1bf4da7fa2d7c62271abb0f13b9e8c
FreePhoenix <freephoenix888@gmail.com>
2021-10-14 13:43:56 +0600

    Update string_insert_input

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/StringInsert.cs
@@ -0,0 +1,10 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    public class StringInsert
+    {
+        public long? id { get; set; }
+        public LinksArrRelInsert? link { get; set; }
+        public long? link_id { get; set; }
+        public string? value { get; set; }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/StringInsertInputType.cs
@@ -0,0 +1,27 @@
+using GraphQL.Types;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    /// <remarks>
+    /// """
+    /// input type for inserting data into table "string"
+    /// """
+    /// input string_insert_input {
+    ///   id: bigint
+    ///   link: links_arr_rel_insert_input
+    ///   link_id: bigint
+    ///   value: String
+    /// }
+    /// </remarks>
+    public class StringInsertInputType : InputObjectGraphType<StringInsert>
+    {
+        public StringInsertInputType()
+        {
+            Name = "string_insert_input";
+            Field(x => x.id, nullable: true, type: typeof(LongGraphType));
+            Field(x => x.link, nullable: true, type: typeof(LinksArrRelInsertInputType));
+            Field(x => x.link_id, nullable: true, type: typeof(LongGraphType));
+            Field(x => x.value, nullable: true, type: typeof(StringGraphType));
+        }
+    }
+}
