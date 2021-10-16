489fed0daddb366d513ba8f42c54da0358b6eba3
FreePhoenix <freephoenix888@gmail.com>
2021-10-16 19:46:09 +0600

    Update mp_constraint

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/MaterializedPathConstraint.cs
@@ -0,0 +1,16 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    /// <remarks>
+    /// """
+    /// unique or primary key constraints on table "mp"
+    /// """
+    /// enum mp_constraint {
+    ///   """unique or primary key constraint"""
+    ///   mp_pkey
+    /// }
+    /// </remarks>
+    public enum MaterializedPathConstraint
+    {
+        mp_pkey
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/MaterializedPathConstraintEnumType.cs
@@ -0,0 +1,11 @@
+namespace Platform.Data.Doublets.Gql.Schema.Types.Enum
+{
+    public class MaterializedPathConstraintEnumType : BaseEnumType<MaterializedPathConstraint>
+    {
+        public MaterializedPathConstraintEnumType() : base("mp_constraint",
+            "unique or primary key constraints on table \"mp\"")
+        {
+
+        }
+    }
+}
