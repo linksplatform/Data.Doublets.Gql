6aeebb3a17a19eb530dfd36112b472a18259c563
FreePhoenix <freephoenix888@gmail.com>
2021-10-16 19:45:11 +0600

    Update string_constraint

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/StringConstraintEnumType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/StringConstraintEnumType.cs
@@ -2,7 +2,7 @@ using GraphQL.Types;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types.Enum
 {
-    public class StringConstraintEnumType : BaseEnumType<string_constraint>
+    public class StringConstraintEnumType : BaseEnumType<StringConstraint>
     {
         public StringConstraintEnumType() : base("string_constraint", "unique or primary key constraints on table \"string\"")
         {
