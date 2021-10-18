9708b81266f6eefa52de020456c50b754381676f
FreePhoenix <freephoenix888@gmail.com>
2021-10-18 14:53:44 +0600

    Update bool_exp_constraint

--- csharp/Platform.Data.Doublets.Gql.Schema/BooleanExpressionConstraint.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/BooleanExpressionConstraint.cs
@@ -1,6 +1,15 @@
 namespace Platform.Data.Doublets.Gql.Schema
 {
-    public enum BooleanExpressionConstraint
+    /// <remarks>
+    /// """
+    /// unique or primary key constraints on table "bool_exp"
+    /// """
+    /// enum bool_exp_constraint {
+    ///   """unique or primary key constraint"""
+    ///   bool_exp_pkey
+    /// }
+    /// </remarks>
+    public enum BoolExpressionConstraint
     {
         bool_exp_pkey
     }
--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/BooleanExpressionConstraintEnumType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Enum/BooleanExpressionConstraintEnumType.cs
@@ -1,7 +1,14 @@
-using GraphQL.Types;
-
 namespace Platform.Data.Doublets.Gql.Schema.Types.Enum
 {
+    /// <remarks>
+    /// """
+    /// unique or primary key constraints on table "bool_exp"
+    /// """
+    /// enum bool_exp_constraint {
+    ///   """unique or primary key constraint"""
+    ///   bool_exp_pkey
+    /// }
+    /// </remarks>
     public class BooleanExpressionConstraintEnumType : BaseEnumType<BoolExpressionConstraint>
     {
         public BooleanExpressionConstraintEnumType() : base("bool_exp_constraint", "unique or primary key constraints on table \"bool_exp\"")
