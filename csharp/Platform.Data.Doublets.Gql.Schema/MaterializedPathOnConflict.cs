df7edb3c65da9c500d4b86236df5e81e67bef617
FreePhoenix <freephoenix888@gmail.com>
2021-10-18 17:19:03 +0600

    Update mp_on_conflict

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/MaterializedPathOnConflict.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    public class MaterializedPathOnConflict
+    {
+        public MaterializedPathConstraint constraint { get; set; }
+        public List<MaterializedPathUpdateColumn> update_columns { get; set; }
+        public MaterializedPathBooleanExpression? where { get; set; }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/MaterializedPathOnConflictInputType.cs
@@ -0,0 +1,27 @@
+using GraphQL.Types;
+using Platform.Data.Doublets.Gql.Schema.Types.Enum;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    /// <remarks>
+    /// """
+    /// on conflict condition type for table "mp"
+    /// """
+    /// input mp_on_conflict {
+    ///   constraint: mp_constraint!
+    ///   update_columns: [mp_update_column!]!
+    ///   where: mp_bool_exp
+    /// }
+    /// </remarks>
+    public class MaterializedPathOnConflictInputType : InputObjectGraphType<MaterializedPathOnConflict>
+    {
+        public MaterializedPathOnConflictInputType()
+        {
+            Name = "mp_on_conflict";
+            Description = "on conflict condition type for table \"mp\"";
+            Field<MaterializedPathConstraintEnumType>("constraint");
+            Field<ListGraphType<NonNullGraphType<MaterializedPathUpdateColumnEnumType>>>("update_columns");
+            Field(x => x.where, nullable: true, type: typeof(MaterializedPathBooleanExpressionInputType));
+        }
+    }
+}
