797afccab85fd079c2acff2193dd3a356f3a1aaf
FreePhoenix <freephoenix888@gmail.com>
2021-10-18 17:03:22 +0600

    Update mp_arr_rel_insert_input

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/MaterializedPathArrayRelationshipInsertInputType.cs
@@ -0,0 +1,24 @@
+using GraphQL.Types;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    /// <remarks>
+    /// """
+    /// input type for inserting array relation for remote table "mp"
+    /// """
+    /// input mp_arr_rel_insert_input {
+    ///   data: [mp_insert_input!]!
+    ///   on_conflict: mp_on_conflict
+    /// }
+    /// </remarks>
+    public class MaterializedPathArrayRelationshipInsertInputType : ObjectGraphType<MaterializedPathArrayRelationshipInsert>
+    {
+        public MaterializedPathArrayRelationshipInsertInputType()
+        {
+            Name = "mp_arr_rel_insert_input";
+            Description = "input type for inserting array relation for remote table \"mp\"";
+            Field<ListGraphType<NonNullGraphType<MaterializedPathArrayRelationshipInsertInputType>>>("data");
+            Field(x => x.on_conflict, nullable: true, type: typeof(MaterializedPathOnConflictInputType));
+        }
+    }
+}
