43c7cd43315e53befd4dc67f1e48e0bec10d960d
FreePhoenix <freephoenix888@gmail.com>
2021-10-23 21:48:51 +0600

    Update mp_max_order_by

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/MaterializedPathMaxOrderByInputType.cs
@@ -0,0 +1,35 @@
+using GraphQL.Types;
+using Platform.Data.Doublets.Gql.Schema.Types.Enum;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    /// <remarks>
+    /// """
+    /// order by max() on columns of table "mp"
+    /// """
+    /// input mp_max_order_by {
+    ///   group_id: order_by
+    ///   id: order_by
+    ///   insert_category: order_by
+    ///   item_id: order_by
+    ///   path_item_depth: order_by
+    ///   path_item_id: order_by
+    ///   position_id: order_by
+    ///   root_id: order_by
+    /// }
+    /// </remarks>
+    public class MaterializedPathMaxOrderByInputType : ObjectGraphType<MaterializedPathMaxOrderBy>
+    {
+        public MaterializedPathMaxOrderByInputType()
+        {
+            Name = "mp_max_order_by";
+            Description = "order by max() on columns of table \"mp\"";
+            Field(x => x.gpoup_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.item_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.path_item_depth, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.path_item_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.root_id, nullable: true, type: typeof(OrderByEnumType));
+        }
+    }
+}
