0ddb4a8a03d29dc009e282ef1f442d40f55105cb
FreePhoenix <freephoenix888@gmail.com>
2021-10-25 19:27:14 +0600

    Update mp_min_order_by

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/MaterializedPathMinOrderBy.cs
@@ -0,0 +1,15 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    public class MaterializedPathMinOrderBy
+    {
+#nullable enable
+        public OrderBy? gpoup_id { get; set; }
+        public OrderBy? id { get; set; }
+        public OrderBy? insert_category { get; set; }
+        public OrderBy? item_id { get; set; }
+        public OrderBy? path_item_depth { get; set; }
+        public OrderBy? path_item_id { get; set; }
+        public OrderBy? position_id { get; set; }
+        public OrderBy? root_id { get; set; }
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/MaterializedPathMinOrderByInputType.cs
@@ -0,0 +1,38 @@
+using GraphQL.Types;
+using Platform.Data.Doublets.Gql.Schema.Types.Enum;
+
+namespace Platform.Data.Doublets.Gql.Schema.Types.Input
+{
+    /// <remarks>
+    /// """
+    /// order by min() on columns of table "mp"
+    /// """
+    /// input mp_min_order_by {
+    ///   group_id: order_by
+    ///   id: order_by
+    ///   insert_category: order_by
+    ///   item_id: order_by
+    ///   path_item_depth: order_by
+    ///   path_item_id: order_by
+    ///   position_id: order_by
+    ///   root_id: order_by
+    /// }
+    /// </remarks>
+    public class MaterializedPathMinOrderByInputType : ObjectGraphType<MaterializedPathMinOrderBy>
+    {
+        public MaterializedPathMinOrderByInputType()
+        {
+            Name = "mp_min_order_by";
+            Description = "order by min() on columns of table \"mp\"";
+            Field(x => x.gpoup_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.insert_category, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.item_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.path_item_depth, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.path_item_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.path_item_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.position_id, nullable: true, type: typeof(OrderByEnumType));
+            Field(x => x.root_id, nullable: true, type: typeof(OrderByEnumType));
+        }
+    }
+}
