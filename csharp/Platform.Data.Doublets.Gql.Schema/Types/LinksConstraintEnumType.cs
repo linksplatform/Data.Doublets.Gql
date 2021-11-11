36ffa75a9d23e02dea691371f856c4acb45681c8
FreePhoenix <freephoenix888@gmail.com>
2021-11-11 13:08:10 +0600

    Remove XML Documentation for private .+

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksConstraintEnumType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/LinksConstraintEnumType.cs
@@ -7,15 +7,6 @@ using System.Threading.Tasks;
 
 namespace Platform.Data.Doublets.Gql.Schema.Types
 {
-    /// <remarks>
-    /// """
-    /// unique or primary key constraints on table "links"
-    /// """
-    /// enum links_constraint {
-    ///   """unique or primary key constraint"""
-    ///   links_pkey
-    /// }
-    /// </remarks>
     class LinksConstraintEnumType : EnumerationGraphType<links_constraint>
     {
         public LinksConstraintEnumType() => Name = "LinksConstraintEnum";
