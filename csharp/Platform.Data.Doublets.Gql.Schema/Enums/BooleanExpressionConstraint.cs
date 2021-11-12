c79d795f36388a39d0bd1fde0ed75228047af461
FreePhoenix <freephoenix888@gmail.com>
2021-11-12 20:39:53 +0600

    Move Enums to the separate folder

---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Enums/BooleanExpressionConstraint.cs
@@ -0,0 +1,16 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    /// <remarks>
+    ///     """
+    ///     unique or primary key constraints on table "bool_exp"
+    ///     """
+    ///     enum bool_exp_constraint {
+    ///     """unique or primary key constraint"""
+    ///     bool_exp_pkey
+    ///     }
+    /// </remarks>
+    public enum BoolExpressionConstraint
+    {
+        bool_exp_pkey
+    }
+}
---
+++ csharp/Platform.Data.Doublets.Gql.Schema/Enums/StringConstraint.cs
@@ -0,0 +1,16 @@
+namespace Platform.Data.Doublets.Gql.Schema
+{
+    /// <remarks>
+    ///     """
+    ///     unique or primary key constraints on table "string"
+    ///     """
+    ///     enum string_constraint {
+    ///     """unique or primary key constraint"""
+    ///     number_pkey
+    ///     }
+    /// </remarks>
+    public enum StringConstraint
+    {
+        string_pkey
+    }
+}
