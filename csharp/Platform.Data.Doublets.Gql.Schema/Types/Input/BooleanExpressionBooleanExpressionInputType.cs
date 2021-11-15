1cfbf48bedccc5d401f61c32093a2dc8bdd66ac2
FreePhoenix <freephoenix888@gmail.com>
2021-11-15 21:29:05 +0600

    Remove hard wrap

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/BooleanExpressionBooleanExpressionInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/BooleanExpressionBooleanExpressionInputType.cs
@@ -9,22 +9,14 @@ namespace Platform.Data.Doublets.Gql.Schema.Types.Input
         public BooleanExpressionBooleanExpressionInputType()
         {
             Name = "bool_exp_bool_exp";
-            Field(x => x._and, true,
-                typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
-            Field(x => x._not, true,
-                typeof(BooleanExpressionBooleanExpressionInputType));
-            Field(x => x._or, true,
-                typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
-            Field(x => x.gql, true,
-                typeof(StringComparisonExpressionInputType));
-            Field(x => x.id, true,
-                typeof(LongComparisonExpressionInputType));
-            Field(x => x.link, true,
-                typeof(LinksBooleanExpressionInputType));
-            Field(x => x.link_id, true,
-                typeof(LongComparisonExpressionInputType));
-            Field(x => x.sql, true,
-                typeof(StringComparisonExpressionInputType));
+            Field(x => x._and, true, typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
+            Field(x => x._not, true, typeof(BooleanExpressionBooleanExpressionInputType));
+            Field(x => x._or, true, typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
+            Field(x => x.gql, true, typeof(StringComparisonExpressionInputType));
+            Field(x => x.id, true, typeof(LongComparisonExpressionInputType));
+            Field(x => x.link, true, typeof(LinksBooleanExpressionInputType));
+            Field(x => x.link_id, true, typeof(LongComparisonExpressionInputType));
+            Field(x => x.sql, true, typeof(StringComparisonExpressionInputType));
         }
     }
 }
