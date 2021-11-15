ced03ae3b162be3dbe0d15bfd0da292d25048097
FreePhoenix <freephoenix888@gmail.com>
2021-11-15 22:14:47 +0600

    Update field syntax

--- csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/BooleanExpressionBooleanExpressionInputType.cs
+++ csharp/Platform.Data.Doublets.Gql.Schema/Types/Input/BooleanExpressionBooleanExpressionInputType.cs
@@ -9,14 +9,14 @@ namespace Platform.Data.Doublets.Gql.Schema.Types.Input
         public BooleanExpressionBooleanExpressionInputType()
         {
             Name = "bool_exp_bool_exp";
-            Field(x => x._and, true, typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
-            Field(x => x._not, true, typeof(BooleanExpressionBooleanExpressionInputType));
-            Field(x => x._or, true, typeof(ListGraphType<BooleanExpressionBooleanExpressionInputType>));
-            Field(x => x.gql, true, typeof(StringComparisonExpressionInputType));
-            Field(x => x.id, true, typeof(LongComparisonExpressionInputType));
-            Field(x => x.link, true, typeof(LinksBooleanExpressionInputType));
-            Field(x => x.link_id, true, typeof(LongComparisonExpressionInputType));
-            Field(x => x.sql, true, typeof(StringComparisonExpressionInputType));
+            Field<ListGraphType<BooleanExpressionBooleanExpressionInputType>>(nameof(MappedType._and));
+            Field<BooleanExpressionBooleanExpressionInputType>(nameof(MappedType._not));
+            Field<ListGraphType<BooleanExpressionBooleanExpressionInputType>>(nameof(MappedType._or));
+            Field<StringComparisonExpressionInputType>(nameof(MappedType.gql));
+            Field<LongComparisonExpressionInputType>(nameof(MappedType.id));
+            Field<LinksBooleanExpressionInputType>(nameof(MappedType.link));
+            Field<LongComparisonExpressionInputType>(nameof(MappedType.link_id));
+            Field<StringComparisonExpressionInputType>(nameof(MappedType.sql));
         }
     }
 }
