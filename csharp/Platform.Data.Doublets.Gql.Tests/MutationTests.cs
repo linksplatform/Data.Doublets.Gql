626f129ab4bbb015f9211e7a0f2da430b2a5cf0a
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 13:46:44 +0600

    Update mutation tests data

--- csharp/Platform.Data.Doublets.Gql.Tests/MutationTests.cs
+++ csharp/Platform.Data.Doublets.Gql.Tests/MutationTests.cs
@@ -55,9 +55,11 @@ namespace Platform.Data.Doublets.Gql.Tests
         [InlineData(@"
         mutation {
           insert_links_one(object: {from_id: 1, to_id: 1}) {
-            id
-            from_id
-            to_id
+            returning{
+              id
+    	        from_id
+    	        to_id
+            }
           }
         }
         ")]
