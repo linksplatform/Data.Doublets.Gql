fe27dfb136091a6f0e8e497f59ebd12968931e95
Ethosa <social.ethosa@gmail.com>
2021-12-06 18:05:14 +0700

    little update

--- python/setup.py
+++ python/setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU Affero General Public License v3.0 (AGPLv3)",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     project_urls={
