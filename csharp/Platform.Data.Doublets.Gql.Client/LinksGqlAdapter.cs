bb65fe17359b6c2aa2c3b95dc368febf4deb0f26
FreePhoenix888 <freephoenix888@gmail.com>
2021-12-11 12:47:27 +0600

    Update LinksGqlAdapter.Update()

--- csharp/Platform.Data.Doublets.Gql.Client/LinksGqlAdapter.cs
+++ csharp/Platform.Data.Doublets.Gql.Client/LinksGqlAdapter.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using Platform.Collections;
 using Platform.Data.Doublets.Gql.Schema;
 using Platform.Data.Doublets.Gql.Schema.Types;
+using Platform.Threading;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
@@ -53,7 +54,31 @@ namespace Platform.Data.Doublets.Gql.Client
             return responseResult.Data.insert_links_one.id;
         }
 
-        public TLink Update(IList<TLink> restrictions, IList<TLink> substitution) => throw new NotImplementedException();
+        public TLink Update(IList<TLink> restrictions, IList<TLink> substitution)
+        {
+            var updateLinkRequest = new GraphQLRequest
+            {
+                Query = @"
+                        mutation UpdateLink ($from_id: Long!, $to_id: Long!, $substitution_from_id: Long!, $substitution_to_id: Long!) {
+                          update_links(_set: { from_id: $substitution_from_id, to_id: $substitution_to_id }, where: { from_id: { _eq: $from_id }, to_id: { _eq: $to_id } }) {
+                            returning {
+                              id
+                            }
+                          }
+                        }",
+                OperationName = "UpdateLink",
+                Variables = new { from_id = restrictions[0], to_id = restrictions[1], substitution_from_id = substitution[0], substitution_to_id = substitution[1] }
+            };
+            var responseResult = _graphQlClient.SendMutationAsync<CreateResponseType>(updateLinkRequest).AwaitResult();
+            if (!responseResult.Errors.IsNullOrEmpty())
+            {
+                foreach (var responseResultError in responseResult.Errors!)
+                {
+                    throw new Exception(responseResultError.Message);
+                }
+            }
+            return responseResult.Data.insert_links_one.id;
+        }
 
         public void Delete(IList<TLink> restrictions) => throw new NotImplementedException();
 
