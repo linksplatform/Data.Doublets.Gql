b34ca68c344b1ae44e6616ee2c0b698b7ae368a5
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-30 21:21:12 +0600

    Update `ClientTests`

--- csharp/Platform.Data.Doublets.Gql.Tests/ClientTests.cs
+++ csharp/Platform.Data.Doublets.Gql.Tests/ClientTests.cs
@@ -1,21 +1,55 @@
 using GraphQL.Client.Http;
-using Platform.Data.Doublets.Gql.Client;
 using GraphQL.Client.Serializer.Newtonsoft;
-using System.Collections.Generic;
+using Platform.Data.Doublets.Gql.Client;
 using Xunit;
-using TLink = System.UInt64;
+using TLinkAddress = System.UInt64;
 
 namespace Platform.Data.Doublets.Gql.Tests;
 
 public class ClientTests
 {
-    [Fact]
-    public void CreateTest()
+    private readonly ulong _any;
+    private readonly LinksConstants<ulong> _linksConstants;
+    private readonly LinksGqlAdapter<ulong> _linksGqlAdapter;
+
+    public ClientTests()
     {
         GraphQLHttpClient graphQlHttpClient = new("http://localhost:60341/v1/graphql", new NewtonsoftJsonSerializer());
-        LinksConstants<TLink> linksConstants = new (true);
-        LinksGqlAdapter<TLink> linksGqlAdapter = new LinksGqlAdapter<ulong>(graphQlHttpClient, linksConstants);
-        var createdLink = linksGqlAdapter.Create(new List<ulong> { 1, 1 });
-        Assert.Equal("(1: 1 1)", createdLink.ToString());
+        _linksConstants = new LinksConstants<ulong>(true);
+        _linksGqlAdapter = new LinksGqlAdapter<ulong>(graphQlHttpClient, _linksConstants);
+        _any = _linksConstants.Any;
+    }
+
+    private void TestCud()
+    {
+        ulong linksAmount = 5;
+        // Create
+        for (ulong i = 1; i <= linksAmount; i++)
+        {
+            ulong one = 1;
+            // Create
+            var createdLink = _linksGqlAdapter.CreateAndUpdate(one, i);
+            // Count
+            Assert.Equal(i, createdLink);
+            Assert.Equal(i, _linksGqlAdapter.Count());
+            Assert.Equal(i, _linksGqlAdapter.Count(one, _any));
+        }
+    }
+
+    [Fact]
+    public void CudTest() => TestCud();
+
+    [Fact]
+    public void EachTest()
+    {
+        TestCud();
+        var count = _linksGqlAdapter.Count();
+        ulong eachIterations = 0;
+        _linksGqlAdapter.Each(link =>
+        {
+            Assert.Equal(++eachIterations, _linksGqlAdapter.GetTarget(link));
+            return _linksConstants.Break;
+        }, new Link<ulong>(_any, _any, _any));
+        Assert.Equal(count, eachIterations);
     }
 }
