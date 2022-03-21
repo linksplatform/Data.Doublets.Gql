44d378287e363443c0af5b5ded63e24699e42616
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-21 17:55:22 +0600

    Update tests

--- csharp/Platform.Data.Doublets.Gql.Tests/MutationTests.cs
+++ csharp/Platform.Data.Doublets.Gql.Tests/MutationTests.cs
@@ -1,11 +1,15 @@
 using GraphQL;
 using GraphQL.SystemTextJson;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Platform.Data.Doublets.Gql.Client;
 using Platform.Data.Doublets.Gql.Schema;
 using Platform.Data.Doublets.Memory;
 using Platform.Data.Doublets.Memory.United.Generic;
 using Platform.IO;
 using Platform.Memory;
+using System;
+using System.Collections.Generic;
 using Xunit;
 using TLinkAddress = System.UInt64;
 
@@ -13,6 +17,7 @@ namespace Platform.Data.Doublets.Gql.Tests
 {
     public class MutationTests
     {
+        public static EqualityComparer<TLinkAddress> EqualityComparer = EqualityComparer<TLinkAddress>.Default;
         public static ILinks<ulong> CreateLinks() => CreateLinks<ulong>(new TemporaryFile());
 
         public static ILinks<TLinkAddress> CreateLinks<TLinkAddress>(string dataDBFilename)
@@ -21,10 +26,6 @@ namespace Platform.Data.Doublets.Gql.Tests
             return new UnitedMemoryLinks<TLinkAddress>(new FileMappedResizableDirectMemory(dataDBFilename), UnitedMemoryLinks<TLinkAddress>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
         }
 
-        [InlineData()]
-        [InlineData()]
-        [InlineData()]
-        [InlineData()]
         [Fact]
         public void InsertLinksOne()
         {
@@ -89,6 +90,7 @@ namespace Platform.Data.Doublets.Gql.Tests
         [Fact]
         public void DeleteLinks()
         {
+
             var links = CreateLinks();
             LinksSchema linksSchema = new(links, new DefaultServiceProvider());
             var jsonTask = linksSchema.ExecuteAsync(_ => { _.Query = @"
@@ -114,12 +116,17 @@ namespace Platform.Data.Doublets.Gql.Tests
             LinksSchema linksSchema = new(links, new DefaultServiceProvider());
             var jsonTask = linksSchema.ExecuteAsync(_ => { _.Query = @"
             mutation {
-              insert_links_one(object: {from_id: 1, to_id: 1}) {
+              insert_links_one(object: {from_id: 0, to_id: 0}) {
                 id
                 from_id
                 to_id
               }
             }
+            "; });
+            var jsonSerializer = new JsonSerializer();
+            var jsonResponse = jsonTask.Result;
+            Assert.False(JObject.Parse(jsonResponse).ContainsKey("errors"));
+            jsonTask = linksSchema.ExecuteAsync(_ => { _.Query = @"
             mutation {
               update_links(_set: { from_id: 1, to_id: 1 }, where: { id: {_eq: 1} }) {
                 returning {
@@ -130,9 +137,10 @@ namespace Platform.Data.Doublets.Gql.Tests
               }
             }
             "; });
-            var response = JObject.Parse(jsonTask.Result);
-            var error = response.ContainsKey("errors");
-            Assert.False(error);
+            jsonResponse = jsonTask.Result;
+            dynamic? responseObject = JsonConvert.DeserializeObject(jsonResponse);
+            Assert.False(JObject.Parse(jsonResponse).ContainsKey("errors"));
+            Assert.True(1 == Convert.ToInt32(responseObject?.data.update_links.returning[0].id));
         }
     }
 }
