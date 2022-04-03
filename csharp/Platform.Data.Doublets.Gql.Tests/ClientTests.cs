1a09fb1ed7a7795cbc8673c5a7e33b5988faf241
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-03 11:21:35 +0600

    Move RunServer and GetEndPoint to text extensions

--- csharp/Platform.Data.Doublets.Gql.Tests/ClientTests.cs
+++ csharp/Platform.Data.Doublets.Gql.Tests/ClientTests.cs
@@ -41,48 +41,7 @@ public class ClientTests : IDisposable
         _serverProcess.Kill(true);
     }
 
-    private Process RunServer()
-    {
-        var currentAssemblyDirectory = Directory.GetCurrentDirectory();
-        var currentProjectDirectory = Path.GetFullPath(Path.Combine(currentAssemblyDirectory, "..", "..", ".."));
-        var serverProjectDirectory = Path.GetFullPath(Path.Combine(currentProjectDirectory, "..", "Platform.Data.Doublets.Gql.Server"));
-        var processStartInfo = new ProcessStartInfo { WorkingDirectory = serverProjectDirectory, FileName = "dotnet", Arguments = $"run -f net5 {TempFilePath}", RedirectStandardOutput = true, RedirectStandardInput = true};
-        var process = Process.Start(processStartInfo);
-        if (null == process || process.HasExited)
-        {
-            throw new Exception("Failed to start server process");
-        }
-        return process;
-    }
 
-    private Uri GetEndPoint(Process process)
-    {
-        while (true)
-        {
-            var standartOutput = process?.StandardOutput;
-            if(standartOutput == null)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                continue;
-            }
-            var processOutputLine = standartOutput.ReadLine();
-            if (string.IsNullOrEmpty(processOutputLine))
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                continue;
-            }
-            if (processOutputLine.Contains("Unable to start"))
-            {
-                throw new Exception("Unable to start.");
-            }
-            if (processOutputLine.Contains("Now listening on: "))
-            {
-                var index = processOutputLine.IndexOf("Now listening on: ") + "Now listening on: ".Length;
-                var uriString = processOutputLine.Substring(index);
-                return new Uri($"{uriString}/v1/graphql");
-            }
-        }
-    }
 
     private void TestCud()
     {
