4fe909878ee20625c58160ee57292b3f83e1b72f
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-20 16:06:48 +0600

    Add returning pub field

--- rust/src/model/object_type/links_mutation_response.rs
+++ rust/src/model/object_type/links_mutation_response.rs
@@ -1,14 +1,17 @@
 use crate::model::Links;
 use async_graphql::*;
 #[derive(Debug)]
-pub struct LinksMutationResponse;
+pub struct LinksMutationResponse {
+    pub returning: Vec<Links>,
+}
 #[Object(name = "links_mutation_response")]
 impl LinksMutationResponse {
     #[graphql(name = "affected_rows")]
     pub async fn affected_rows(&self, ctx: &Context<'_>) -> i32 {
         todo!()
     }
-    pub async fn returning(&self, ctx: &Context<'_>) -> Vec<Links> {
-        todo!()
+
+    pub async fn returning(&self, ctx: &Context<'_>) -> &[Links] {
+        &self.returning
     }
 }
